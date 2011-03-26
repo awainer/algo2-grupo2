a2ed6fbe4b2bb4ccefd42f89bb4496c7eb72bbbb
awainer@gmail.com <awainer@gmail.com@3e978e55-bc30-f19f-1413-e70aa2f8f0d4>
2011-03-26 03:50:25 +0000

    reescribo pushToken

--- tp1/a_lexico.c
+++ tp1/a_lexico.c
@@ -4,7 +4,8 @@
 #include <string.h>
 
 
-int TLexico_Crear(TLexico* al){
+int TLexico_Crear(TLexico* al)
+{
     al->error_codigo=0;
     al->buffer_idx=-1;
     al->token->tipo=TOKEN_NULL;
@@ -12,57 +13,86 @@ int TLexico_Crear(TLexico* al){
     return 0;
 }
 
-int TLexico_setAnalizadorSintactico(TLexico* al, TSintactico* as){
+int TLexico_setAnalizadorSintactico(TLexico* al, TSintactico* as)
+{
     al-> sintactico = as;
     return 0;
 }
 
-int TLexico_PushChar(TLexico* al, char c){
 
-    /* el hecho de que C no soporte rangos hace esta implementacion mas bien engorrosa*/
-/*if ( (( c >= 'a' )&&( c <= 'z' ) ) || ( (c>='A') && (c<='Z') )){
-    token.tipo=TOKEN_STRING;
-	}*/
-	/*token.tipo=TOKEN_FALSE;*/
-
-        if  (c=='"'){
-            if (al->buffer_idx==-1){
-                /*comienza un string*/
-                al->buffer_idx=0;
-                }
-
-                else{ /* estoy recibiendo una comilla pero el buffer ya tenia datos, asi que
-                  estoy terminando el string*/
-                    al->buffer_idx=-1; /*reinicio el index*/
-                    if (TSintactico_PushToken(al->sintactico,al->token)){
-                        return 0;
-                        }
-                    else{
-                    /*Analizador Sintactico me devolvio un error*/
-                        al->error_codigo=1;
-                        /*TODO cambiar esto por un strcpy
-                        al->error_mensaje="el analizador sintactico me dio error";*/
-                        return 2;
-                        }
-        }
-
-if (al->buffer_idx > -1){
-        if ( (( c >= 'a' )&&( c <= 'z' ) ) || ( (c>='A') && (c<='Z') ))
+int TLexico_PushChar(TLexico* al, char c)
+{
+    /*Recibo comillas*/
+    if (c=='"')
+    {
+        if (al->buffer_idx==-1)
         {
-
-            al->token->dato[al->buffer_idx]=c;
-            al->buffer_idx++;
+        /* Si recibo una comilla pero el indice esta en -1,
+        significa que comienza un string*/
+        al->token->tipo=TOKEN_STRING;
+        al->buffer_idx++;
+        }
+        else
+        {
+            /* Si recibo una comilla con el indice distingo de -1,
+            tengo que terminar el string y resetear */
+            al->buffer_idx=-1;
+            al->token->tipo=TOKEN_NULL;
+            if (TSintactico_PushToken(al->sintactico,al->token))
+            {
+                return 0;
+            }
+            else
+            {
+                al->error_codigo=1;
+                strcpy(al->error_mensaje,"El asintactico me devolvio error");
+            }
         }
 
-        if((c>='0') && (c<='9')) {
+    }
+    /*Recibo letras */
+    else if ( (( c >= 'a' )&&( c <= 'z' ) ) || ( (c>='A') && (c<='Z') ))
+    {
+        al->token->dato[al->buffer_idx]=c;
+        al->buffer_idx++;
+    }
+    /*Recibo numeros*/
+    else if((c>='0') && (c<='9'))
+    {
+        /*Voy a asumir que no me van a mandar
+        fruta en el medio de un numero*/
+      al->buffer_idx++;
+      al->token->tipo=TOKEN_NUMERO;
+      al->token->dato[al->buffer_idx]=c;
 
-            al->token->tipo=TOKEN_NUMERO;
-            al->token->dato[al->buffer_idx]=c;
-            al->buffer_idx++;
+    }
+    /*Recibo comas*/
+    else if(c==',')
+    {
+        if(al->token->tipo==TOKEN_NUMERO)
+        {
+            /*Si recibo una coma cuando estaba recibiendo un numero, termina
+            En ese caso, tengo que mandar el token del numero y el de la coma*/
+            if (TSintactico_PushToken(al->sintactico,al->token))
+            {
+                al->token->tipo=TOKEN_COMA;
+                if (TSintactico_PushToken(al->sintactico,al->token))
+                {
+                    /* Esto lo hago como ifs anidados porque si lo pongo como
+                    un solo if de dos condiciones no se cual va a ejecutar primero*/
+                al->buffer_idx=-1;
+                al->token->tipo=TOKEN_NULL;
+                return 0;
+                }
+            }
+            else
+            {
+                al->error_codigo=1;
+                strcpy(al->error_mensaje,"El asintactico me devolvio error");
+            }
         }
-}
+        else  if(c=='{')
 
-        if(c=='{')
             al->token->tipo=TOKEN_OBJETO_EMPIEZA;
         else if(c=='}')
             al->token->tipo=TOKEN_OBJETO_TERMINA;
@@ -70,32 +100,40 @@ if (al->buffer_idx > -1){
             al->token->tipo=TOKEN_ARRAY_EMPIEZA;
         else if(c==']')
             al->token->tipo=TOKEN_ARRAY_TERMINA;
-        else if(c==',')
-            al->token->tipo=TOKEN_COMA;
-        else if(c==',')
+        else if(c==':')
             al->token->tipo=TOKEN_DOSPUNTOS;
-        else{
-            al->error_codigo=1;
-           /* al->error_mensaje="El caracter recibido no tiene un token asociado";*/
-           strcpy(al->error_mensaje,"Caracter no valido");
-           return 1;
+        else
+        {
+            al->error_codigo=2;
+            strcpy(al->error_mensaje,"Caracter no valido");
+            return al->error_codigo;
         }
-    if(al->token->tipo==TOKEN_FALSE)
-        return 0;
-    else{
-        TSintactico_PushToken(al->sintactico,al->token);
+
+        /* Ahora mandamos tokens atomicos*/
+        if (TSintactico_PushToken(al->sintactico,al->token))
+            return 0;
+        else
+         {
+           al->error_codigo=1;
+           strcpy(al->error_mensaje,"El asintactico me devolvio error");
+         }
+
     }
- }
-return 0;
+
+
+ /*fin de PushChar no deberia llegar nunca aca, deberia retornar antes*/
+return 4;
 
 }
 
 
-int TLexico_terminarFlujo(TLexico* al){
+int TLexico_terminarFlujo(TLexico* al)
+{
     return 0;
 }
 
-int TLexico_getUltimoError(TLexico* al, int  * codigo, char* mensaje){
+int TLexico_getUltimoError(TLexico* al, int  * codigo, char* mensaje)
+{
     return 0;
 }
 
