d54b42363cab7631e94cb6d548e1fa7a3229ccfc
fabi.ilc <fabi.ilc@3e978e55-bc30-f19f-1413-e70aa2f8f0d4>
2011-03-30 01:55:14 +0000

    commiteo el a_sintactico.c,, agrego los casos de NUMERO,COMA Y 2 PUNTOS,  tambien deje varios comentarios con cosas para ver

--- trunk/tp1/a_sintactico.c
+++ trunk/tp1/a_sintactico.c
@@ -19,42 +19,76 @@ int TSintactico_PushToken(TSintactico* as, Token* token){
         switch (token->tipo){
         case TOKEN_STRING:  while (i<=2)
                                 if((as->tValidos[i] == token->tipo) && (as->Tstring == 1)){
-                                    as->tValidos=[TOKEN_DOSPUNTOS,TOKEN_NULL]
-                                    printf("clave");
+                                    as->tValidos = [TOKEN_DOSPUNTOS,TOKEN_NULL];  //el token null habria que sacarlo porque al ser null termina y queda algo incorrecto sintacticamente.
+                                    printf("clave");                               // en los tvalidos habria que agregar el token_coma
                                     as->Tstring = 2;
                                     }
-                                    else
+                                    else {
                                         i++;
                             if(i == 2)
-                                printf(("error"));
+                                printf(("error"));}
                             break;
 
 
 
 
 
-        case TOKEN_NUMERO,
+        case TOKEN_NUMERO:  while ( i <= 2 )
+                                if (as->tValidos[i] == token->tipo) {
+                                    as->tValidos=[TOKEN_COMA]; // otra vez no estoy seguro de poner el TOKEN_NULL "dammit"
+                                    //imprime el tipo aca con un printf(token->tipo) ?
+                                }
+                                else {
+                                    i++;
+                                    if (i == 3)
+                                       prinft( "error" );}
+                                       break;
+
+
+
+
         case TOKEN_OBJETO_EMPIEZA:   while (i<=1)
                                             if(as->tValidos[i] == token->tipo){
-                                                as->tValidos=[TOKEN_OBJETO_TERMINA,TOKEN_STRING,TOKEN_NULL]
+                                                as->tValidos=[TOKEN_OBJETO_TERMINA,TOKEN_STRING,TOKEN_NULL];
                                                 printf("objeto\n\t");
+                                                // ¿Aca deberia imprimir el token ademas de escribir objeto?
                                             }
-                                        else
+                                        else {
                                             i++;
                                      if(i == 2)
-                                        printf(("error"));
+                                        printf(("error"));} // ver as->error_codigo
                                      break;
 
         case TOKEN_OBJETO_TERMINA,
         case TOKEN_ARRAY_EMPIEZA,
         case TOKEN_ARRAY_TERMINA,
-        case TOKEN_COMA,
-        case TOKEN_DOSPUNTOS,
+        case TOKEN_COMA:   while ( i <= 1 )
+                                if (as->tValidos[i] == token->tipo) {
+                                    as->tValidos = [TOKEN_STRING];
+                                    printf( " , " );
+                                }
+                                else {
+                                     i++;
+                                     if ( i == 2 )
+                                     printf( "error" ); }
+                                     break;
+
+
+        case TOKEN_DOSPUNTOS:  while ( i <= 1 )
+                                    if (as->tValidos[i] == token->tipo) {
+                                        as->tValidos = [TOKEN_STRING,TOKEN_NUMERO,TOKEN_ARRAY_EMPIEZA];  // no estoy seguro de que vaya el token_null,si va acordate de modificar el numero del while
+                                        printf( " : " ) ;}
+                                    else
+                                        i++;
+                                     if (i == 2)
+                                     printf( "error" ) ; // ver as->error_codigo
+                                     break;
+
         case TOKEN_TRUE,
         case TOKEN_FALSE,
 
-        */
 
+*/
 
     return 0;
     }
