f2892d9c0cf8acb0f40c63d68b8ca1919d7747aa
awainer@gmail.com <awainer@gmail.com@3e978e55-bc30-f19f-1413-e70aa2f8f0d4>
2011-04-09 22:58:59 +0000

    Reescribo el parser para cumplir con la interface del enunciado, ademas cambio los valores de error del sintactico para que sean E_SINTACTICO

--- tp1/parser.c
+++ tp1/parser.c
@@ -3,69 +3,38 @@
 #include <stdio.h>
 #include <string.h>
 
-int TParser_Crear(TSintactico*  ts,TLexico * tl,char * archivo, TParser * parser){
-	parser->as=ts;
-	parser->lex=tl;
-    parser->ultimoError=E_NONE;
-    parser->fh=fopen(archivo,"r");
-
-    printf("El archivo es %s\n",archivo);
-    if(parser->fh==NULL)
-    {   printf("No puedo abrir el archivo %s",archivo);
-        exit(5);
-    }
-    TLexico_Crear(parser->lex);
-    TSintactico_Crear(parser->as);
-    TLexico_setAnalizadorSintactico(parser->lex,parser->as);
-
-    return 0;
-}
-int TParser_Parsear(TParser * parser){
-    parser->c=getc(parser->fh);
-    while((parser->c!=EOF) && (parser->ultimoError == E_NONE))
-    {
-      /*  printf("%c\n",parser->c);*/
-        parser->ultimoError=TLexico_PushChar(parser->lex,parser->c);
-        parser->c=getc(parser->fh);
-
-    }
-    /*printf("%s %d",parser->lex->error_mensaje,parser->c);*/
-    return TParser_TerminarFlujo(parser);
-
-}
 
 
-int TParser_TerminarFlujo(TParser * p)
+
+int TParser_Crear(TParser* p)
 {
-    return TLexico_terminarFlujo(p->lex);
+    TLexico_Crear(&p->lex);
+    TSintactico_Crear(&p->as);
+    TLexico_setAnalizadorSintactico(&p->lex,&p->as);
+    p->ultimoError=E_NONE;
+    return 0;
 }
 
-int TParser_getUltimoError(TParser* p, int * codigo, char  mensaje[50])
+int TParser_PushChar(TParser* p, char c)
 {
+    return TLexico_PushChar(&p->lex,c);
 
-    *codigo=p->lex->error_codigo;
-
-    switch(*codigo)
-    {
-
-    case E_NONE :        {
-                         strcpy(mensaje,"No ha ocurrido ningun error");
-                         break; /* Aca esta justificado el uso del break porque no puede tener dos codigos distintos */
-                         }
-
-    case E_LEXICO :      { /* error en el lexico*/
-                         strcpy(mensaje,p->lex->error_mensaje);
-                         break;
-                         }
-
-    case E_SINTACTICO :  { /* error en el sintactico*/
-                         strcpy(mensaje,p->as->error_mensaje);
-                         break;
-                         }
-    }
-    return 0;
 }
-int TParser_Destruir(TParser * parser){
-    fclose(parser->fh);
-    return 0;
+
+int TParser_terminarFlujo(TParser* p)
+{
+    return TLexico_terminarFlujo(&p->lex);
+}
+
+
+int TParser_getUltimoError(TParser* p, int * codigo, char* mensaje)
+{
+    TLexico_getUltimoError(&p->lex,codigo,mensaje);
+    if(*codigo==E_LEXICO)
+        return 0;
+    else
+    {
+        TSintactico_getUltimoError(&p->as,codigo,mensaje);
+        return 0;
+    }
 }
