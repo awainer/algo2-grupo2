9ab3a6785a61b635c68de872fc7132da40c2ede3
fabi.ilc@gmail.com <fabi.ilc@gmail.com@3e978e55-bc30-f19f-1413-e70aa2f8f0d4>
2011-04-20 18:36:07 +0000

    Comienzo el constr_tweets,  primitivas de crear y destruir

---
+++ trunk/tp2/constr_tweets.c
@@ -0,0 +1,30 @@
+#include "a_sintactico.h"
+#include "cola.h"
+#include "dict.h"
+
+
+
+
+
+
+int Tconstructor_Crear(Tconstructor* tc); {
+
+C_Crear(&tc->pcola,2*sizeof(int*));
+tc->Ecorrecto=0;
+tc->diccionario->elementos = NULL ; /* no hay elementos*/
+tc->diccionario->cantidad = 0; /* esta vacio */
+return 0;
+}
+
+int Tconstructor_Destruir(TConstructor* tc);{
+
+
+}
+
+
+int Tconstructor_setCola(TConstructor *tc, TCola* cola);
+
+
+
+int TConstructor_eventoOcurrido (TConstructor *tc, int evento, void* dato);
+
