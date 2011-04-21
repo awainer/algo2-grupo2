11e1d4e50c7deb69f1bd9a0e46bffcef2f2e41d6
joako.crespo <joako.crespo@3e978e55-bc30-f19f-1413-e70aa2f8f0d4>
2011-04-21 18:43:51 +0000

    implemente la pila, al final se podia usar la metodologia de antes adaptada y es mucho mas simple q antes. esta todo comentado el sintact igual x q no sabia ni donde crear las variables ni como pasarselas bien ala pila. tema q tengo q hablar con ari este sabado cuando nos juntemos todos.  Ari si podes hechale una mirada.

--- tp2/a_sintactico.c
+++ tp2/a_sintactico.c
@@ -5,15 +5,23 @@
 #include "parser.h"
 #include <stdio.h>
 #include <string.h>
+#include "pila.h"
+#define TamanioDato sizeof(int)
 
 
-
-int TSintactico_Crear(TSintactico* as){
+/*int TSintactico_Crear(TSintactico* as){
+    struct Tpila * pP;
     as->error_codigo=0;
     as->estado[0]=NADA;
     as->estado_idx=1;
     as->pos=1;
     as->tab=0;
+
+
+
+    P_Crear(&pP, TamanioDato); /creo la pila y le paso el tamanio dl dato que seria enumerado osea int/
+    P_Poner(&pP, NADA)          /pongo como primer elemento NADA/
+
 
     return 0;
     }
@@ -21,41 +29,32 @@ int TSintactico_Crear(TSintactico* as){
 int TSintacticoCasoValor(TSintactico * as,Token * token){
 
     switch (token->tipo){
-		case TOKEN_STRING:  as->estado[as->estado_idx]=VALOR;
-                            TSintacticoImpimir(as, token);        /*agregar caso al switch para imprimir*/
-                            as->pos=0;
+		case TOKEN_STRING:
+                            P_Poner(&pP, VALOR);
+                            TSintacticoImpimir(as, token);        /*agregar caso al switch para imprimir/
                             return 0;
-		case TOKEN_NUMERO:  as->estado[as->estado_idx]=VALOR;
-                            TSintacticoImpimir(as,token);        /*agregar caso al switch para imprimir*/
-                            as->pos=0;
+		case TOKEN_NUMERO:
+                            P_Poner(&pP, VALOR);
+                            TSintacticoImpimir(as,token);        /*agregar caso al switch para imprimir/
                             return 0;
 
 
-		case TOKEN_OBJETO_EMPIEZA:  as->estado[as->estado_idx]=OBJETO;
-                                    TSintacticoImpimir(as, token);        /*agregar caso al switch para imprimir*/
-                                    as->estado_idx++;
-                                    as->estado[as->estado_idx]=-1;
-                                    as->pos=1;
+		case TOKEN_OBJETO_EMPIEZA:  P_Poner(&pP, OBJETO);
+                                    TSintacticoImpimir(as, token);        /*agregar caso al switch para imprimir/
                                     return 0;
 
-		case TOKEN_ARRAY_EMPIEZA:   as->estado[as->estado_idx]=ARRAY;
-                                    TSintacticoImpimir(as, token);        /*agregar caso al switch para imprimir*/
-                                    as->estado_idx++;
-                                    as->estado[as->estado_idx]=-1;
-                                    as->pos=1;
+		case TOKEN_ARRAY_EMPIEZA:   P_Poner(&pP, ARRAY);
+                                    TSintacticoImpimir(as, token);        /*agregar caso al switch para imprimir/
                                     return 0;
-		case TOKEN_TRUE:           as->estado[as->estado_idx]=VALOR;
-                                    TSintacticoImpimir(as,token);        /*agregar caso al switch para imprimir*/
-                                    as->pos=0;
+		case TOKEN_TRUE:            P_Poner(&pP, VALOR);
+                                    TSintacticoImpimir(as,token);        /*agregar caso al switch para imprimir/
                                     return 0;
-		case TOKEN_FALSE:         as->estado[as->estado_idx]=VALOR;
-                                    TSintacticoImpimir(as,token);        /*agregar caso al switch para imprimir*/
-                                    as->pos=0;
+		case TOKEN_FALSE:           P_Poner(&pP, VALOR);
+                                    TSintacticoImpimir(as,token);        /*agregar caso al switch para imprimir/
                                     return 0;
 
-		case TOKEN_NULL:         as->estado[as->estado_idx]=VALOR;
-                                    TSintacticoImpimir(as,token);        /*agregar caso al switch para imprimir*/
-                                    as->pos=0;
+		case TOKEN_NULL:            P_Poner(&pP, VALOR);
+                                    TSintacticoImpimir(as,token);        /*agregar caso al switch para imprimir/
                                     return 0;
 		default : { strcpy(as->error_mensaje,"se esperaba un valor");
                 as->error_codigo=E_SINTACTICO;
@@ -224,32 +223,28 @@ int TSintacticoImpimir(TSintactico * as, Token * token){
 
 int TSintactico_PushToken(TSintactico* as, Token* token)
 {
-/*	printf("Recibo un token de tipo %d y dato %s\n",token->tipo,token->dato);*/
+    P_Sacar(%pP, &pE)
+    EleAux=pE;
+    P_Poner(&pP, &pE);
 
-	 /*Este codigo es para debug, agregado por Ari*/
 
 
+    /*la pila solo avanza cuando se inicia asi queda NADA en la pos 0 y cuando recibe un array y objeto empieza/
+    /*si vienen claves valores comas o dospuntos reescribe en la misma pos/
 
-    /*la pila solo avanza cuando se inicia asi queda NADA en la pos 0 y cuando recibe un array y objeto empieza*/
-    /*si vienen claves valores comas o dospuntos reescribe en la misma pos*/
-
-	if(as->estado[as->estado_idx-as->pos]==NADA)
+	if(EleAux==NADA)
 	{
-	    /*Estos son los tokens validos como primeros tokens */
+	    /*Estos son los tokens validos como primeros tokens /
 	    if (token->tipo==TOKEN_ARRAY_EMPIEZA)
          {
-            as->estado[as->estado_idx]=ARRAY;
+            P_Poner(&pP, ARRAY);
             TSintacticoImpimir(as,token);
-            as->estado_idx++;
-
             return 0;
          }
         else if (token->tipo==TOKEN_OBJETO_EMPIEZA)
         {
-            as->estado[as->estado_idx]=OBJETO;
+            P_Poner(&pP, OBJETO) ;
             TSintacticoImpimir(as,token);
-            as->estado_idx++;
-
             return 0;
         }
         else {
@@ -260,39 +255,37 @@ int TSintactico_PushToken(TSintactico* as, Token* token)
 
 	}
 
-if(as->estado[as->estado_idx-as->pos]==OBJETO)
+if(EleAux==OBJETO)
 {
-    /*Estos son los tokens validos si lo ultimo que hize fue abrir un objeto */
+    /*Estos son los tokens validos si lo ultimo que hize fue abrir un objeto /
     if(token->tipo==TOKEN_STRING)
     {
-           /*esto es una clave*/
-           as->estado[as->estado_idx]=CLAVE;
-           as->pos=0;
-           TSintacticoImpimir(as, token); /*llamar a otra funcion para imprimir tipo clave*/
+           /*esto es una clave/
+           P_Poner(&pP, CLAVE)
+           TSintacticoImpimir(as, token); /*llamar a otra funcion para imprimir tipo clave/
 
            return 0;
     }
     else if (token->tipo==TOKEN_OBJETO_TERMINA)
             {
                 TSintacticoImpimir(as,token);
-                as->estado_idx--;                                   /* si viene un objeto_cierra, y antes tenia un objeto, entonces vuelvo una posicion */
-                if (as->estado[as->estado_idx-1]== ARRAY)           /*si antes tenia un array entonces estaba en un valor*/
+                P_Sacar(&pP, &Pe);                                  /saco de la cola el OBJETO/
+                P_Sacar(&pP, &Pe);                        /saco de la cola lo que hay en el tope para ver en que estado estoy/
+                EleAux=Pe;
+                P_Poner(&pP,Pe)
+                if (EleAux== ARRAY)           /*si antes tenia un array entonces estaba en un valor/
                 {
-                    as->estado[as->estado_idx]=VALOR;
-                    as->pos=0;
-
+                    P_Poner(&pP,VALOR)
                     return 0;
                 }
-                else if (as->estado[as->estado_idx-1]== OBJETO)         /*y sino estaba en un objeto osea que tmb estaba en un valor */
+                else if (EleAux== OBJETO)         /*y sino estaba en un objeto osea que tmb estaba en un valor /
                     {
-                        as->estado[as->estado_idx]=VALOR;
-                        as->pos=0;
-
+                        P_Poner(&pP,VALOR)
                         return 0;
                     }
-                else    /*el estado anterior era NADA entonces todo lo q se abrio se cerro*/
+                else    /*el estado anterior era NADA entonces todo lo q se abrio se cerro/
                 {
-                    as->estado[as->estado_idx]=NADA;
+                    EleAux=NADA;
 
                     return 0;
 
@@ -305,13 +298,14 @@ if(as->estado[as->estado_idx-as->pos]==OBJETO)
         return as->error_codigo;
     }
 }
-/* Si lo ultimo recibido fue una clave, ahora tengo que recibir un dospuntos*/
-if(as->estado[as->estado_idx]==CLAVE)
+/* Si lo ultimo recibido fue una clave, ahora tengo que recibir un dospuntos/
+if(EleAux==CLAVE)
 {
     if(token->tipo==TOKEN_DOSPUNTOS)
     {
-      as->estado[as->estado_idx]=DOSP;
-      TSintacticoImpimir(as,token);        /*agregar caso al switch*/
+      P_Sacar(&pP, &Pe);
+      P_Poner(&pP, DOSP)
+      TSintacticoImpimir(as,token);        /*agregar caso al switch/
       return 0;
     }
     else
@@ -321,30 +315,29 @@ if(as->estado[as->estado_idx]==CLAVE)
         return as->error_codigo;
     }
 }
-/* Si lo ultimo recibido fue un array, ahora tengo que recibir un valor*/
- if (as->estado[as->estado_idx-as->pos]== ARRAY)
+/* Si lo ultimo recibido fue un array, ahora tengo que recibir un valor/
+ if (EleAux== ARRAY)
 {
     if ((token->tipo==TOKEN_ARRAY_TERMINA))
     {
-        as->estado_idx--;
+        P_Sacar(&pP, &Pe);
+        P_Sacar(&pP, &Pe);
+        EleAux=Pe;
+        P_Poner(&pP, &Pe);
         TSintacticoImpimir(as,token);
-        if (as->estado[as->estado_idx-1]== ARRAY)           /*si antes tenia un array entonces estaba en un valor*/
+        if (EleAux== ARRAY)           /*si antes tenia un array entonces estaba en un valor/
         {
-            as->estado[as->estado_idx]=VALOR;
-            as->pos=0;
-
+            P_Poner(&pP, VALOR);
             return 0;
         }
-        else if (as->estado[as->estado_idx-1]== OBJETO)      /*y sino estaba en un objeto osea que tmb estaba en un valor */
+        else if (EleAux== OBJETO)      /*y sino estaba en un objeto osea que tmb estaba en un valor /
         {
-            as->estado[as->estado_idx]=VALOR;
-            as->pos=0;
-
+            P_Poner(&pP, VALOR);
             return 0;
         }
-        else    /*el estado anterior era NADA entonces todo lo q se abrio se cerro*/
+        else    /*el estado anterior era NADA entonces todo lo q se abrio se cerro/
         {
-            as->estado[as->estado_idx]=NADA;
+            EleAux=NADA;
 
             return 0;
         }
@@ -357,88 +350,107 @@ if(as->estado[as->estado_idx]==CLAVE)
     }
      return 0;
 }
-/*si lo ultimo recibido fue un dospuntos , ahora tengo q recibir un valor*/
- if (as->estado[as->estado_idx]== DOSP)
+/*si lo ultimo recibido fue un dospuntos , ahora tengo q recibir un valor/
+ if (EleAux== DOSP)
 {
-     TSintacticoCasoValor(as,token);
-     return 0;
+    P_Sacar(&pP, &Pe);
+    TSintacticoCasoValor(as,token);
+    return 0;
 }
- /*si lo ultimo recibido fue un valor, ahora tengo q recibir una coma o un fin de objeto o fin de array respectivamente*/
- if (as->estado[as->estado_idx]== VALOR)
+ /*si lo ultimo recibido fue un valor, ahora tengo q recibir una coma o un fin de objeto o fin de array respectivamente/
+ if (EleAux== VALOR)
  {
-     if(token->tipo==TOKEN_COMA)
+    if(token->tipo==TOKEN_COMA)
     {
-        as->estado[as->estado_idx]=COMA;
-        TSintacticoImpimir(as,token);        /*agregar caso al switch*/
+        P_Sacar(&pP, &Pe);
+        P_Poner(&pP, COMA);
+        TSintacticoImpimir(as,token);
         return 0;
-    }
-    else if ((token->tipo==TOKEN_OBJETO_TERMINA) && (as->estado[as->estado_idx-1]== OBJETO))
-    {
-
-        TSintacticoImpimir(as,token);
-        as->estado_idx--;                                   /* si viene un objeto_cierra, y antes tenia un objeto, entonces vuelvo una posicion */
-        if (as->estado[as->estado_idx-1]== ARRAY)           /*si antes tenia un array entonces estaba en un valor*/
-        {
-            as->estado[as->estado_idx]=VALOR;
-
-            return 0;
-        }
-        else if (as->estado[as->estado_idx-1]== OBJETO)         /*y sino estaba en un objeto osea que tmb estaba en un valor */
-        {
-            as->estado[as->estado_idx]=VALOR;
-
-            return 0;
-        }
-        else    /*el estado anterior era NADA entonces todo lo q se abrio se cerro*/
-        {
-            as->estado[as->estado_idx]=NADA;
-
-            return 0;
-
-        }
-    }
-    else if ((token->tipo==TOKEN_ARRAY_TERMINA) && (as->estado[as->estado_idx-1]== ARRAY))
-    {
-        as->estado_idx--;
-        TSintacticoImpimir(as,token);
-        if (as->estado[as->estado_idx-1]== ARRAY)           /*si antes tenia un array entonces estaba en un valor*/
-        {
-            as->estado[as->estado_idx]=VALOR;
-
-            return 0;
-        }
-        else if (as->estado[as->estado_idx-1]== OBJETO)      /*y sino estaba en un objeto osea que tmb estaba en un valor */
-        {
-            as->estado[as->estado_idx]=VALOR;
-
-            return 0;
-        }
-        else    /*el estado anterior era NADA entonces todo lo q se abrio se cerro*/
-        {
-            as->estado[as->estado_idx]=NADA;
-
-            return 0;
-        }
-
     }
     else
     {
-        strcpy(as->error_mensaje,"Se esperaba una coma, un fin de array o un fin de objeto");
-        as->error_codigo=E_SINTACTICO;
-        return as->error_codigo;
+        P_Sacar(&pP, &Pe);
+        P_Sacar(&pP, &Pe);
+        EleAux=Pe;
+        P_Poner(&pP, &Pe);
+        P_Poner(&pP, &Pe);
+        if ((token->tipo==TOKEN_OBJETO_TERMINA) && (EleAux== OBJETO))
+        {
+
+            TSintacticoImpimir(as,token);
+            P_Sacar(&pP, &Pe);
+            P_Sacar(&pP, &Pe);
+            EleAux=Pe;
+            P_Poner(&pP, &Pe);                               /* si viene un objeto_cierra, y antes tenia un objeto, entonces vuelvo una posicion /
+            if (EleAux== ARRAY)           /*si antes tenia un array entonces estaba en un valor/
+            {
+                P_Poner(&pP, VALOR);
+
+                return 0;
+            }
+            else if (EleAux== OBJETO)         /*y sino estaba en un objeto osea que tmb estaba en un valor /
+            {
+                P_Poner(&pP, VALOR);
+
+                return 0;
+            }
+            else    /*el estado anterior era NADA entonces todo lo q se abrio se cerro/
+            {
+                EleAux=NADA;
+
+                return 0;
+
+            }
+        }
+        else if ((token->tipo==TOKEN_ARRAY_TERMINA) && EleAux== ARRAY))
+        {
+            TSintacticoImpimir(as,token);
+            P_Sacar(&pP, &Pe);
+            P_Sacar(&pP, &Pe);
+            EleAux=Pe;
+            P_Poner(&pP, &Pe);
+            if (EleAux]== ARRAY)           /*si antes tenia un array entonces estaba en un valor/
+            {
+                P_Poner(&pP, VALOR);
+
+                return 0;
+            }
+            else if (EleAux== OBJETO)      /*y sino estaba en un objeto osea que tmb estaba en un valor /
+            {
+                P_Poner(&pP, VALOR);
+
+                return 0;
+            }
+            else    /*el estado anterior era NADA entonces todo lo q se abrio se cerro/
+            {
+                EleAux=NADA;
+
+                return 0;
+            }
+
+        }
+        else
+        {
+            strcpy(as->error_mensaje,"Se esperaba una coma, un fin de array o un fin de objeto");
+            as->error_codigo=E_SINTACTICO;
+            return as->error_codigo;
+        }
     }
  }
-
- /*si lo ultimo recibido fue una coma, ahora tengo q recibir una clave o un valor depende si estaba en un objeto o en un array*/
- if (as->estado[as->estado_idx]== COMA)
+ /*si lo ultimo recibido fue una coma, ahora tengo q recibir una clave o un valor depende si estaba en un objeto o en un array/
+ if (EleAux== COMA)
  {
-     if (as->estado[(as->estado_idx-1)]==OBJETO)
+    P_Sacar(&pP, &Pe);
+    P_Sacar(&pP, &Pe);
+    EleAux=Pe;
+    P_Poner(&pP, &Pe);
+    if (EleAux==OBJETO)
      {
          if(token->tipo==TOKEN_STRING)
        {
-           /*esto es una clave*/
-           as->estado[as->estado_idx]=CLAVE;
-           TSintacticoImpimir(as, token); /*llamar a otra funcion para imprimir tipo clave*/
+           /*esto es una clave/
+           P_Poner(&pP, CLAVE);
+           TSintacticoImpimir(as, token); /*llamar a otra funcion para imprimir tipo clave/
            return 0;
        }
         else
@@ -448,7 +460,7 @@ if(as->estado[as->estado_idx]==CLAVE)
           return as->error_codigo;
        }
      }
-     else if (as->estado[(as->estado_idx-1)]==ARRAY)
+     else if (EleAux==ARRAY)
      {
          TSintacticoCasoValor(as,token);
      }
@@ -456,7 +468,7 @@ if(as->estado[as->estado_idx]==CLAVE)
 return as->error_codigo;
 }
 
-
+*/
 int TSintactico_terminarFlujo(TSintactico* as)
 {
 
@@ -484,8 +496,6 @@ int TSintactico_getUltimoError(TSintactico*  as, int *codigo, char* mensaje){
     strcpy(mensaje,as->error_mensaje);
     return 0;
     }
-
 int TSintactico_setCallback (TSintactico *ts, int evento, void* tda_contexto, int(*callback)(const void*, int, const void*)){
 
 return NULL;
-}
