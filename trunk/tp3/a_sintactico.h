1cd5e75b34911f7c461ccc153a51177a53458cad
awainer@gmail.com <awainer@gmail.com@3e978e55-bc30-f19f-1413-e70aa2f8f0d4>
2011-05-21 22:14:06 +0000

    copio tp2 en tp3

---
+++ trunk/tp3/a_sintactico.h
@@ -0,0 +1,110 @@
+#ifndef A_SINTACTICO_H_INCLUDED
+#define A_SINTACTICO_H_INCLUDED
+#include "Pila.h"
+#include "token.h"
+#include "constr_tweets.h"
+typedef enum
+{
+    /*enumerado de callbacks*/
+    CB_COMIENZA_ARRAY,
+    CB_TERMINA_ARRAY,
+    CB_COMIENZA_OBJETO,
+    CB_TERMINA_OBJETO,
+    CB_CLAVE,
+    CB_TRUE,
+    CB_FALSE,
+    CB_NULL,
+    CB_NUMERO,
+    CB_STRING
+} evento;
+typedef enum
+{
+    /*enumerado para manejarnos dentro de la pseudopila para soportar los anidamientos*/
+    NADA,
+    OBJETO,
+    CLAVE,
+    VALOR,
+    ARRAY,
+    DOSP,
+    COMA
+} TipoEstado;
+
+typedef struct _tcallback{
+         void* contexto;
+         int (*cb) ( void * tc,  void  * dato);
+} tcallback;
+
+typedef struct tda_sintactico
+{
+    /* El ultimo error recibido, 0 es ok. */
+    int  error_codigo;
+    char error_mensaje[30];
+    /*una variable donde guardare los estados q no irian en la pila(clave valor etc)*/
+    int  estado;
+    /*guardo los estados antes de ponerlos en la pila(objeto array)*/
+    int estpila;
+    TPila   pP;
+
+    tcallback arrayCallbacks[10];
+    /*evento * callbacks[10];*/
+    /*int (*callbacks[10]) (Tconstructor * tc, void * dato);
+    Tconstructor * constructor;*/
+    /* comento esto para mostrar mi idea
+     Estos son los tokens validos como clave
+       TipoToken tValidos[4]; indica los tokens validos que pueden venir. nose x q lo borraron
+    int Vtoken[4];  indica en que situacion estoy con un flag en el token adecuado*/
+
+} TSintactico;
+
+
+
+
+/*
+Descripción: Inicializa una nueva variable de tipo TSintactico
+Precondiciones: as apunta a una variable de tipo TSintactico no creada previamente.
+Postcondiciones: Se inicializó la variable as*/
+int TSintactico_Crear(TSintactico* as);
+/*
+Descripción: Coloca un nuevo token para ser analizado, mostrando por pantalla los
+mensajes correspondientes.
+Precondiciones: as esta inicializado.
+Postcondiciones: Se muestran los mensajes en pantalla. En caso de un error sintáctico se
+devuelve un valor de error, y el mismo puede ser consultado mediante la primitiva
+getUltimoError
+*/
+int TSintactico_PushToken(TSintactico* as, Token* tk);
+/*
+Descripción: Indica que se terminó el documento JSON actual, se reinicializa el estado
+interno del TDA para procesar un nuevo JSON.
+Precondiciones: as esta inicializado.
+Postcondiciones: Se reinició el TDA para procesar otro flujo. Si el flujo terminó en un
+estado inconsistente se devuelve un error. La forma de obtener la descripción del error es la
+misma que en el caso de pushToken.*/
+int TSintactico_terminarFlujo(TSintactico* as);
+/*
+Descripción: Devuelve una descripción mas detallada del ultimo error ocurrido.
+Precondiciones: as esta inicializado. Ocurrió un error al procesar un flujo de tokens.
+Postcondiciones: Se devuelve un código de error y en mensaje un mensaje descriptivo del
+mismo.
+*/
+int TSintactico_getUltimoError(TSintactico*  as, int *codigo, char* mensaje);
+
+/*
+Descripción: Forma de comunicarse con el exterior. Se establece ante la ocurrencie del evento "evento". De haber sido definida previamente se reemplaza
+por esta nueva.
+Precondiciones: ts ha sido creado.
+Postcondiciones: se establece la callback a llamar ante la ocurrencia del evento "evento". Si ya se había definido la callback se reemplaza por esta
+nueva.
+*/
+int TSintactico_setCallback(TSintactico *ts, int evento, void* tda_contexto, int(*callback)(const void*, int, const void*));
+
+/*Descripción: Libera los recursos asociados del analizador sintactico.
+Precondiciones: El analizador sintactico ha sido creado.
+Postcondiciones: Se liberan recursos asociados.*/
+
+int Tsintactico_Destruir(TSintactico *as );
+
+int Tsintactico_setCallback(TSintactico *ts, int evento, void* tda_contexto, int(*callback)(  void  *,  void*) );
+
+/*int Tsintactico_setCallback(TSintactico *ts, int evento, void* tda_contexto, void(*callback)(const void*, const void*));*/
+#endif
---
+++ trunk/tp3/main.c
@@ -0,0 +1,172 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "token.h"
+#include "a_sintactico.h"
+#include "a_lexico.h"
+#include "parser.h"
+#include "dict.h"
+#include "constr_tweets.h"
+#include "LsO.h"
+
+
+int comparar_dicts(void* v1, void* v2)
+{
+    TDiccionario *d1,*d2;
+    int aux;
+    char * buffer1, * buffer2;
+
+    d1=(TDiccionario*)v1;
+    d2=(TDiccionario*)v2;
+
+    aux=TDiccionario_sizeDato(d1,"user_screen_name");
+    buffer1=(char*)malloc(aux);
+    TDiccionario_obtener(d1,"user_screen_name",buffer1);
+    aux=TDiccionario_sizeDato(d2,"user_screen_name");
+    buffer2=(char*)malloc(aux);
+    TDiccionario_obtener(d2,"user_screen_name",buffer2);
+    aux=strcmp(buffer1,buffer2);
+    free(buffer1);
+    free(buffer2);
+    return aux;
+
+}
+
+
+
+int main(int argc, char * argv[])
+{
+
+
+    TDiccionario * miDiccionario=NULL;
+    TCola colaTweets;
+    Tconstructor miConstructor;
+    TParser miParser;
+    TListaOrdenada miListaOrdenada;
+    int flag;
+    FILE *  archivo;
+    char    c;
+    int    error=E_NONE,sizeDato,i=0;
+
+    char * nuevo=NULL, * actual=NULL;
+
+    /*int  i,cant_elementos ;*/
+
+
+    /*Inicializo el constructor con su cola*/
+    Tconstructor_Crear(&miConstructor);
+    C_Crear(&colaTweets,sizeof(TDiccionario));
+    Tconstructor_setCola(&miConstructor,&colaTweets);
+    TParser_Crear(&miParser);
+
+    TParser_setCallback(&miParser,CB_COMIENZA_ARRAY,&miConstructor,Tconstructor_eventoComienzaArray);
+    TParser_setCallback(&miParser,CB_TERMINA_ARRAY,&miConstructor,Tconstructor_eventoTerminaArray);
+    TParser_setCallback(&miParser,CB_COMIENZA_OBJETO,&miConstructor,Tconstructor_eventoComienzaObjeto);
+    TParser_setCallback(&miParser,CB_TERMINA_OBJETO,&miConstructor,Tconstructor_eventoTerminaObjeto);
+    TParser_setCallback(&miParser,CB_CLAVE,&miConstructor,Tconstructor_eventoClave);
+    TParser_setCallback(&miParser,CB_TRUE,&miConstructor,Tconstructor_eventoTrue);
+    TParser_setCallback(&miParser,CB_FALSE,&miConstructor,Tconstructor_eventoFalse);
+    TParser_setCallback(&miParser,CB_NULL,&miConstructor,Tconstructor_eventoNull);
+    TParser_setCallback(&miParser,CB_NUMERO,&miConstructor,Tconstructor_eventoNumero);
+    TParser_setCallback(&miParser,CB_STRING,&miConstructor,Tconstructor_eventoString);
+
+    archivo=fopen(argv[1],"r");
+
+    if(archivo==NULL)
+    {
+        printf("No puedo abrir el archivo\n");
+        return -1;
+    }
+
+ c=getc(archivo);
+
+
+
+    while( (c!=EOF) && (error==E_NONE) )
+    {
+        error=TParser_PushChar(&miParser,c);
+        c=getc(archivo);
+
+
+    }
+
+
+if(error==E_NONE)
+{
+    /*Saco de la cola y meto en la lista ordenada*/
+    miDiccionario=(TDiccionario*)malloc(sizeof(TDiccionario));
+    LO_Crear(&miListaOrdenada,sizeof(TDiccionario),comparar_dicts);
+    while ( !C_Vacia(colaTweets)) {
+        C_Sacar(&colaTweets,miDiccionario);
+        LO_Insertar(&miListaOrdenada,miDiccionario);
+        }
+
+if(!LO_Vacia(miListaOrdenada))
+{
+
+        LO_Mover_Cte(&miListaOrdenada,L_Primero);
+        LO_Elem_Cte(miListaOrdenada,miDiccionario);
+        sizeDato=TDiccionario_sizeDato(miDiccionario,"user_screen_name");
+
+        actual=(char *)malloc(sizeDato);
+        TDiccionario_obtener(miDiccionario,"user_screen_name",actual);
+        /*nuevo=(char *)malloc(sizeDato);
+        TDiccionario_obtener(miDiccionario,"user_screen_name",nuevo);*/
+
+            flag=TRUE;
+            while(flag)
+            {
+                i=0;
+                /*free(actual);*/
+               if(nuevo!=NULL)
+                    free(nuevo);
+                nuevo=(char *)malloc(strlen(actual)+1);
+                TDiccionario_obtener(miDiccionario,"user_screen_name",nuevo);
+
+                while(flag && (!strcmp(actual,nuevo)))
+                {                        i++;
+
+                    flag=LO_Mover_Cte(&miListaOrdenada,L_Siguiente);
+                    if(flag)
+                    {
+                        free(actual);
+                        LO_Elem_Cte(miListaOrdenada,miDiccionario);
+                        sizeDato=TDiccionario_sizeDato(miDiccionario,"user_screen_name");
+                        actual=(char *)malloc(sizeDato);
+                        TDiccionario_obtener(miDiccionario,"user_screen_name",actual);
+                    }
+                }
+               if(i>=2)
+                    printf(" %s %d\n",nuevo,i);
+
+            }
+
+}
+}
+
+    LO_Mover_Cte(&miListaOrdenada,L_Primero);
+    do{
+        LO_Elem_Cte(miListaOrdenada,miDiccionario);
+        Tdiccionario_Destruir(miDiccionario);
+
+    }while(LO_Mover_Cte(&miListaOrdenada,L_Siguiente));
+
+
+    LO_Destruir(&miListaOrdenada);
+
+
+        if(nuevo!=NULL)
+            free(nuevo);
+        if(actual!=NULL)
+            free(actual);
+
+    free(miDiccionario);
+    fclose(archivo);
+    Tconstructor_Destruir(&miConstructor);
+    TParser_destruir(&miParser);
+
+    return 0;
+
+}
+
