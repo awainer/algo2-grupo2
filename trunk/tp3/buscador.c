66d78d39ff354ecefaa10a79f4f6ea2538715fa4
awainer@gmail.com <awainer@gmail.com@3e978e55-bc30-f19f-1413-e70aa2f8f0d4>
2011-05-25 03:27:37 +0000

    agrego estucturas para nuevos TDA

---
+++ trunk/tp3/buscador.c
@@ -0,0 +1,21 @@
+#include "buscador.h"
+
+int Tbuscador_crear(Tbuscador* tb, TTokenizer* tt, TIndice* ti)
+{
+    return 0;
+}
+
+int Tbuscador_destruir(Tbuscador* tb)
+{
+    return 0;
+}
+
+int Tbuscador_union(Tbuscador* tb, char* frase, TListaSimple * docs)
+{
+    return 0;
+}
+
+int Tbuscador_interseccion(Tbuscador* tb, char* frase, TListaSimple * docs)
+{
+    return 0;
+}
---
+++ trunk/tp3/buscador.h
@@ -0,0 +1,38 @@
+#ifndef BUSCADOR_H_INCLUDED
+#define BUSCADOR_H_INCLUDED
+
+#include "tokenizer.h"
+#include "indice.h"
+#include "lista.h"
+
+/**TDA Buscador (Tbuscador)**/
+typedef struct{
+    TTokenizer * tk;
+    TIndice    * ti;
+}Tbuscador;
+
+
+/*pre: ninguna
+post: el TDA Buscador ha sido creado. Tt es un puntero al
+tokenizer que se usará para analizar los strings de búsqueda*/
+int Tbuscador_crear(Tbuscador* tb, TTokenizer* tt, TIndice* ti);
+/*pre: el TDA Buscador ha sido creado
+post: los recursos del Buscador fueron liberados*/
+int Tbuscador_destruir(Tbuscador* tb);
+/*pre: el TDA Buscador ha sido creado. Docs es una lista de
+Tdiccionarios* que va a contener los Tweets
+post: se devuelve el conjunto de documentos que cumple con alguna
+de las palabras de la frase. La frase es tokenizada previamente
+mediante el TDA tokenizer. Los Tweets resultado se devuelven en la
+lista docs.*/
+int Tbuscador_union(Tbuscador* tb, char* frase, TListaSimple * docs);
+/*pre: el TDA Buscador ha sido creado. Docs es una lista de
+TDiccionario* que va a tener los Tweets
+post: se devuelve el conjunto de documentos que cumple con todas
+de las palabras de la frase. La frase es tokenizada previamente
+mediante el TDA tokenizer. Los Tweets resultado se devuelven en la
+lista docs.*/
+int Tbuscador_interseccion(Tbuscador* tb, char* frase, TListaSimple * docs);
+
+
+#endif
