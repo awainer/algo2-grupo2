c806dd93c76edf69122c561c8e8f9136fdd4eecc
pgcassanelli1989@gmail.com <pgcassanelli1989@gmail.com@3e978e55-bc30-f19f-1413-e70aa2f8f0d4>
2011-06-07 23:22:03 +0000

    Commiteo lo que vengo haciendo del buscador.c. Saludos!

--- tp3/buscador.c
+++ tp3/buscador.c
@@ -2,6 +2,9 @@
 
 int Tbuscador_crear(Tbuscador* tb, TTokenizer* tt, TIndice* ti)
 {
+    tb->tk=tt;
+    tb->ti=ti;
+
     return 0;
 }
 
@@ -12,10 +15,51 @@ int Tbuscador_destruir(Tbuscador* tb)
 
 int Tbuscador_union(Tbuscador* tb, char* frase, TListaSimple * docs)
 {
+    TListaSimple* terminosAux /* Puntero donde se cargaran  los punteros TDiccionario que contengan el término. */
+    terminosAux = (TListaSimple*) malloc(sizeof(TListaSimple));
+
+    char * fraseAux;
+
+    Ttokenizer_analizar(&tb->tk, frase, terminosAux);
+
+    L_Mover_Cte(&terminosAux, L_Primero);
+    L_Elem_Cte(terminosAux, fraseAux);
+
+    while(!(fraseAux==NULL)){
+        Tindice_listarDocs(&tb->ti, fraseAux, docs);
+        L_Mover_Cte(&terminosAux, L_Siguiente);
+        L_Elem_Cte(terminosAux,fraseAux);
+    }
+
+    free(terminosAux);
+
     return 0;
 }
 
 int Tbuscador_interseccion(Tbuscador* tb, char* frase, TListaSimple * docs)
 {
+    TListaSimple* terminosAux; /* Puntero donde se cargaran  los punteros TDiccionario que contengan el término. */
+    terminosAux = (TListaSimple*) malloc(sizeof(TListaSimple));
+    int VACIO; /* Si la lista de tweets esta vacia devuelve 1, sino 0. */
+    char * fraseAux;
+
+    Ttokenizer_analizar(&tb->tk, frase, terminosAux);
+
+    L_Mover_Cte(&terminosAux, L_Primero);
+    L_Elem_Cte(terminosAux, fraseAux);
+
+    while(!(fraseAux==NULL)){
+        Tindice_listarDocs(&tb->ti, fraseAux, docs);
+        if (!((L_Mover_Cte(&docs, L_Siguiente)==NULL))){
+            L_Mover_Cte(&terminosAux, L_Siguiente);
+            L_Elem_Cte(terminosAux,fraseAux);
+        }else if ((L_Mover_Cte(&docs, L_Siguiente))==NULL){
+            L_Mover_Cte(&docs, L_Primero);
+            /* Como no se cumplen con TODAS las palabras de la frase, vacio la lista docs */
+            while (!(L_Mover_Cte(&docs, L_Siguiente)==NULL))
+                L_Borrar_Cte(&docs);
+            }
+    }
+    free(terminosAux);
     return 0;
 }
